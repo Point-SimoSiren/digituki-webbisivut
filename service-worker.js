b84c9c5fd50066f1b3341bdca78fe693d9188ae2
Simppa <simo.siren@careeria.fi>
2025-12-06 00:27:14 +0200

    leikkimistä

--- service-worker.js
+++ service-worker.js
@@ -1,21 +1,31 @@
-const CACHE_NAME = 'code-porvoo-v1';
+const CACHE_NAME = 'code-porvoo-v2';
 const FILES_TO_CACHE = [
   '/',
   '/index.html',
-  '/Laptop.png',
-  '/TuxiC.png',
-  '/manifest.json'
+  '/images/Laptop.png',
+  '/images/TuxiC.png',
+  '/manifest.json',
 ];
 
-self.addEventListener('install', event => {
+self.addEventListener('install', (event) => {
   event.waitUntil(
-    caches.open(CACHE_NAME).then(cache => cache.addAll(FILES_TO_CACHE))
+    caches.open(CACHE_NAME).then((cache) => cache.addAll(FILES_TO_CACHE)),
   );
 });
 
-self.addEventListener('fetch', event => {
+self.addEventListener('activate', (event) => {
+  event.waitUntil(
+    caches
+      .keys()
+      .then((keys) =>
+        Promise.all(keys.filter((key) => key !== CACHE_NAME).map((key) => caches.delete(key))),
+      )
+      .then(() => self.clients.claim()),
+  );
+});
+
+self.addEventListener('fetch', (event) => {
   event.respondWith(
-    caches.match(event.request)
-      .then(response => response || fetch(event.request))
+    caches.match(event.request).then((response) => response || fetch(event.request)),
   );
 });
